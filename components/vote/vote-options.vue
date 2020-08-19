d7ceb53a975e07de78694b68267886f5670394fb
Tommy Cox <tommy.cox@protonmail.com>
2020-08-18 19:56:25 -0600

    feat: vote options list UI scaffolding

---
+++ components/vote/vote-options.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-layout justify-center>
+    <v-simple-table fixed-header height="300px" style="width: 500px">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Project
+            </th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="project in projects" :key="project.name">
+            <td>{{ project.name }}</td>
+            <td class="text-center"><v-btn>Vote</v-btn></td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237
+        },
+        {
+          name: 'Eclair',
+          calories: 262
+        },
+        {
+          name: 'Cupcake',
+          calories: 305
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375
+        },
+        {
+          name: 'Lollipop',
+          calories: 392
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408
+        },
+        {
+          name: 'Donut',
+          calories: 452
+        },
+        {
+          name: 'KitKat',
+          calories: 518
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/vote.vue
+++ pages/vote.vue
@@ -1,19 +1,15 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
+  <v-layout justify-center>
+    <VoteOptions />
   </v-layout>
 </template>
 
 <script>
-export default {}
+import VoteOptions from '~/components/vote/vote-options'
+
+export default {
+  components: {
+    VoteOptions
+  }
+}
 </script>
