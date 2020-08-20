6cb0d7fcc512a56cd9404784f8d0323354375907
Tommy Cox <tommy.cox@protonmail.com>
2020-08-19 23:02:40 -0600

    feat: detect existence of eddsa private keys in local storage to skip sign up

---
+++ components/sign-up/generate-keys.vue
@@ -0,0 +1,1 @@
+// TODO: Refactor key generation into this single component.
--- middleware/auth.js
+++ middleware/auth.js
@@ -8,14 +8,14 @@ export default function({ store, redirect, route }) {
   } else if (
     store.state.account &&
     store.state.hasAccess && // Has the access NFT
-    !store.state.hasKeys && // But doesn't have eddsa keys
+    !store.state.localStorage.validKeys.privKey && // But doesn't have eddsa keys
     route.name !== 'sign-up' // And isn't on the sign-up page
   ) {
     redirect('/sign-up')
   } else if (
     store.state.account &&
     store.state.hasAccess && // Has the access NFT
-    store.state.hasKeys && // And has eddsa keys
+    store.state.localStorage.validKeys.privKey && // And has eddsa keys
     route.name !== 'vote' // And isn't on the vote page
   ) {
     redirect('/vote')
